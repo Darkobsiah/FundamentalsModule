bbdec3989cc8857e1186b97cd5eec8703e97a0c1
slavcho179 <slavidimitrov06@abv.bg>
2023-09-28 18:45:57 +0300

    Uploading Course Problem Solved

---
+++ ListBasics/course.py
@@ -0,0 +1,9 @@
+# Read User input
+number_of_courses = int(input())
+my_list = []
+# Logic
+for course in range(number_of_courses):
+    my_list.append(input())
+
+print(my_list)
+# Print User output
