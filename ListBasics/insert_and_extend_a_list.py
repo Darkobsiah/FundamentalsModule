9277f01943f61056a04ab257363d20482594f97b
slavcho179 <slavidimitrov06@abv.bg>
2023-09-28 18:39:03 +0300

    uploading an example of extend and insert functions

---
+++ ListBasics/insert_and_extend_a_list.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 3]
+
+# First parameter is the index
+# The second is the number we insert at that exact index
+my_list.insert(3, 4)
+
+# We extend our list with these integers
+my_list.extend([5, 6, 7])
+
+print(my_list)
