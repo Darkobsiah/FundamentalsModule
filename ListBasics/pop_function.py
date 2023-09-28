2e75720afe57fe068b85da4ede7460132d5509e7
slavcho179 <slavidimitrov06@abv.bg>
2023-09-28 18:32:48 +0300

    uploading an example of pop function

---
+++ ListBasics/pop_function.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 3, 4, 10]
+
+# Taking the last element by default
+popped_element = my_list.pop()
+
+# Indice from 0
+popped_element_by_indices = my_list.pop(0)
+
+print(popped_element)
+print(popped_element_by_indices)
