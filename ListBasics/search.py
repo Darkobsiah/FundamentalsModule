9db31db036e0e892a1fa8439b5d5e35c48a65659
slavcho179 <slavidimitrov06@abv.bg>
2023-09-29 23:06:43 +0300

    Uploading search 100/100

--- ListBasics/search.py
+++ ListBasics/search.py
@@ -1,15 +1,15 @@
 # Read User input
 n = int(input())
 special = input()
-# Logic
 my_list = []
 special_list = []
+# Logic
 for line in range(n):
     string = input()
     my_list.append(string)
 print(my_list)
-for line in range(len(my_list)):
-    if 'SoftUni' in my_list[line]:
-        special_list.append(my_list[line])
+for i in range(len(my_list)):
+    if special in my_list[i]:
+        special_list.append(my_list[i])
 print(special_list)
 # Print User output
