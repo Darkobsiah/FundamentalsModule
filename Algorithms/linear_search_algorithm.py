d231f20293862fd9be4b2749dfd2de9a89f83fa8
slavcho179 <slavidimitrov06@abv.bg>
2023-09-30 16:13:15 +0300

    Uploading linear search algorithm

--- Algorithms/linear_search_algorithm.py
+++ Algorithms/linear_search_algorithm.py
@@ -1,16 +1,17 @@
-# Read User input
-def linear_search(input_list, element):
-    for i in range(len(my_list)):
-        if my_list[i] == element:
-            result = element
-            return i
+# Def Linear Searching Function
+def linear_searching(your_list, element):
+    for i in range(len(your_list)):
+        if your_list[i] == element:
+            result = i
+            return result
     return -1
 
-my_list = [1, 3, 5, 7, 9, 11, 13]
-element = 14
+# Read Input
+mylist = [int(x) for x in input().split(',')]
+target = 7
 
-result = linear_search(my_list, element)
-if result != -1:
-    print(f'The target element {element} is at index {result}')
-else:
-    print(f'The target element {element} was not found in the list')
+# Call the function
+result = linear_searching(mylist, target)
+
+# Print function return
+print(result)
