10139fed72fee3966473652a0935ee0cdcc669e5
slavcho179 <slavidimitrov06@abv.bg>
2023-10-01 17:14:59 +0300

    Uploading index function usability

---
+++ ListBasics/index_function.py
@@ -0,0 +1,5 @@
+my_list = [1, 2, 3, 4, 5, 6, 'gosho']
+
+# We print certain element's index by:
+print(my_list.index(2))
+print(my_list.index('gosho'))
--- ListBasics/indexing_through_a_list.py
+++ ListBasics/indexing_through_a_list.py
@@ -2,4 +2,4 @@ my_list = [1, 2, 3, 4, 5, 6, 7]
 
 print(my_list)
 for index in range(len(my_list)):
-    print(f'Index {index} - element{my_list[index]}')
+    print(f'Index {index} - element {my_list[index]}')
