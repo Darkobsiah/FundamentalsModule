23ec56eb6f4be643f51e3b09c8624a2c8b70d5ea
slavcho179 <slavidimitrov06@abv.bg>
2023-10-05 19:26:37 +0300

    Seize the fire 100/100, Finally!

--- ListBasics/seize_the_fire.py
+++ ListBasics/seize_the_fire.py
@@ -6,27 +6,28 @@ total_fire = 0
 put_out_cells = []
 
 print('Cells:')
-
+# Logic
 for fire in fires:
     args = fire.split(' = ')
-    fire_style = args[0]
-    amount = int(args[1])
+    fire_type = args[0]
+    level = int(args[1])
     valid = False
-    if water < amount:
+
+    if water < level:
         continue
 
-    if fire_style == 'High' and 81 < amount < 125:
+    if fire_type == 'High' and 81 <= level <= 125:
         valid = True
-    elif fire_style == 'Medium' and 51 < amount < 80:
+    elif fire_type == 'Medium' and 51 <= level <= 80:
         valid = True
-    elif fire_style == 'Low' and 1 < amount < 50:
+    elif fire_type == 'Low' and 1 <= level <= 50:
         valid = True
 
     if valid:
-        put_out_cells.append(amount)
-        water -= amount
-        effort += amount * 0.25
-        total_fire += amount
+        put_out_cells.append(level)
+        water -= level
+        total_fire += level
+        effort += level * 0.25
 
 for each in put_out_cells:
     print(f' - {each}')
