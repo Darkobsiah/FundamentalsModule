11a846cd1354363ed5283b91d548035c66128a6e
slavcho179 <slavidimitrov06@abv.bg>
2023-10-05 19:18:49 +0300

    Seize the fire 30/100

--- ListBasics/seize_the_fire.py
+++ ListBasics/seize_the_fire.py
@@ -1,42 +1,35 @@
-total_effort = 0
-type_and_values = []
-list_of_values = []
-type_and_values = input().split('#')
-amount_of_water = int(input())
-length = (len(type_and_values))
-counter = 0
-# Logic
-for cell in type_and_values:
-    element = cell.split(' = ')
-    type_of_fire = element[0]
-    integer_cell = element[1]
-    current_amount = 0
-    value_of_cell = int(integer_cell)
-    if type_of_fire == 'High' and 81 < int(value_of_cell) < 125:
-        if amount_of_water > value_of_cell:
-            amount_of_water -= value_of_cell
-            current_amount += value_of_cell * 0.25
-            list_of_values.append(value_of_cell)
+fires = input().split('#')
+water = int(input())
 
-    elif type_of_fire == 'Medium' and 51 < value_of_cell < 80:
-        if amount_of_water > value_of_cell:
-            amount_of_water -= value_of_cell
-            current_amount += value_of_cell * 0.25
-            list_of_values.append(value_of_cell)
+effort = 0
+total_fire = 0
+put_out_cells = []
 
-    elif type_of_fire == 'Low' and 1 < value_of_cell < 50:
-        if amount_of_water > value_of_cell:
-            amount_of_water -= value_of_cell
-            current_amount += value_of_cell * 0.25
-            list_of_values.append(value_of_cell)
-    total_effort += current_amount
-    counter += 1
-    if cell == type_and_values[-1]: # Is it last?
-        break
-# Print the outputs
 print('Cells:')
-for each in list_of_values:
-    print(' -', each)
-print(f'Effort: {total_effort:.2f}')
-total = sum(list_of_values)
-print(f'Total Fire: {total}')
+
+for fire in fires:
+    args = fire.split(' = ')
+    fire_style = args[0]
+    amount = int(args[1])
+    valid = False
+    if water < amount:
+        continue
+
+    if fire_style == 'High' and 81 < amount < 125:
+        valid = True
+    elif fire_style == 'Medium' and 51 < amount < 80:
+        valid = True
+    elif fire_style == 'Low' and 1 < amount < 50:
+        valid = True
+
+    if valid:
+        put_out_cells.append(amount)
+        water -= amount
+        effort += amount * 0.25
+        total_fire += amount
+
+for each in put_out_cells:
+    print(f' - {each}')
+print(f'Effort: {effort:.2f}')
+print(f'Total Fire: {total_fire}')
+
