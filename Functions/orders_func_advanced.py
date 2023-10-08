e17e8345fa81d2deb3db5363714246a2b3220a99
slavcho179 <slavidimitrov06@abv.bg>
2023-10-08 18:56:19 +0300

    Uploading orders function ADVANCED solution

---
+++ Functions/orders_func_advanced.py
@@ -0,0 +1,14 @@
+# Define a function
+def orders(product: str, quantity: int):
+    return {
+        'coffee': quantity * 1.5,
+        'water': quantity * 1,
+        'coke': quantity * 1.4,
+        'snacks': quantity * 2
+    }.get(product)
+
+item = input()
+quantity = int(input())
+result = orders(item, quantity)
+print(f"{result:.2f}")
+# Read User input
