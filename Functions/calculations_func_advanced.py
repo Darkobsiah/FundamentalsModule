1282edc847263dcee07f8a0d9095f7e59dcd2d55
slavcho179 <slavidimitrov06@abv.bg>
2023-10-08 19:04:55 +0300

    Uploading a calculating function with ADVANCED solution

---
+++ Functions/calculations_func_advanced.py
@@ -0,0 +1,19 @@
+# Define a function
+def calculations(operator: str, a: int, b: int):
+    """
+    This function uses an operator to make calculations
+    between the numbers a and b
+
+    :return: the result from the operation
+    """
+    return {
+        "multiply": a * b,
+        "divide": int(a / b),
+        "add": a + b,
+        "subtract": a - b,
+    }.get(operator)
+# Read User input
+operator = input()
+a = int(input())
+b = int(input())
+print(calculations(operator, a, b))
