b4a95814a272e363510ff61dd37de4b781a52858
slavcho179 <slavidimitrov06@abv.bg>
2023-10-10 19:19:49 +0300

    Uploading odd and even sum function

--- Functions/odd_and_even_sum.py
+++ Functions/odd_and_even_sum.py
@@ -1,11 +1,23 @@
 # Define a function
 def odd_and_even_sum(number: int):
     """
-
-    :param number:
-    :return:
+    This function returns the sum of all even
+    and all odd digits in a given number.
+    :param number: int
+    :return: 2 integers
     """
+    sum_of_odd = 0
+    sum_of_even = 0
+    for num in str(number):
+        if int(num) % 2 == 0:
+            sum_of_even += int(num)
+        else:
+            sum_of_odd += int(num)
+    return sum_of_odd, sum_of_even
 
 # Read User input
+number_special = int(input())
+sum_of_odd_digits, sum_of_even_digits = odd_and_even_sum(number_special)
 
-# Print functiont return
+# Print function return
+print(f'Odd sum = {sum_of_odd_digits}, Even sum = {sum_of_even_digits}')
