1f7e4ef2c8c56b92e06273d2bb44c72f6c6ea411
slavcho179 <slavidimitrov06@abv.bg>
2023-10-11 22:42:51 +0300

    Uploading finished function for password validation

--- Functions/password_validator.py
+++ Functions/password_validator.py
@@ -1,23 +1,32 @@
 # Define a function
-def password_validator(special: str):
+def password_validator(user_pass: str):
     valid = True
-    if 6 > len(special) > 10:
+    special = user_pass.lower()
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    integer_counter = 0
+    if 6 > len(special) or 10 < len(special):
+        print('Password must be between 6 and 10 characters')
         valid = False
-
-    # Make the letter or digits check
     for symbol in special:
-        if 0 < ord(symbol) < 47:
-            valid = False
-        if 65 < ord(symbol) < 90:
-            valid = False
+        if symbol not in alphabet:
+            if symbol not in numbers:
+                print('Password must consist only of letters and digits')
+                valid = False
+        if symbol in numbers:
+            integer_counter += 1
 
-    # Check for 2 digits at least
-    return True
-
+    if integer_counter < 2:
+        print('Password must have at least 2 digits')
+        valid = False
+    if valid:
+        return 'Password is valid'
+    else:
+        return ''
 
 
 # Read User input
 password = input()
 
-
 # Print the result of the function
+print(password_validator(password))
