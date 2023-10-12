d3a80b9ecc03a707f920ad9014e12fe1c6482929
slavcho179 <slavidimitrov06@abv.bg>
2023-10-12 19:46:52 +0300

    Uploading multiplication sing

---
+++ Functions/multiplication_sing.py
@@ -0,0 +1,17 @@
+# Read User input
+numbers_list = [int(input()), int(input()), int(input())]
+negative_count = 0
+
+
+for number in numbers_list:
+    if number < 0:
+        negative_count += 1
+
+if 0 in numbers_list:
+    result = "zero"
+elif negative_count == 0 or negative_count == 2:
+    result = "positive"
+else:
+    result = "negative"
+
+print(result)
