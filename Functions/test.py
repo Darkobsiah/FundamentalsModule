aa9b3d817f3a7ce34f6be53549bfbf8b7bac2673
slavcho179 <slavidimitrov06@abv.bg>
2023-10-12 19:47:34 +0300

    Updating test file

--- Functions/test.py
+++ Functions/test.py
@@ -1,2 +1,8 @@
-def something():
-    return
+def palindrome_checker(list):
+    for num in list:
+        is_palindrome = num[0] == num[-1]
+        print(is_palindrome)
+
+
+positive_ints = input().split(", ")
+print(palindrome_checker(positive_ints))
