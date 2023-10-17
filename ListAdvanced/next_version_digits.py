6ad90fd385547744e91a0db085d76235a59e1290
slavcho179 <slavidimitrov06@abv.bg>
2023-10-17 17:50:28 +0300

    Uploading list advanced

--- ListAdvanced/next_version_digits.py
+++ ListAdvanced/next_version_digits.py
@@ -1,4 +1,4 @@
 version = input()
 version_int = version.replace(".", "")
-next_version = int(version_int) + 1
-print(".".join(str(next_version)))
+new_version = int(version_int) + 1
+print(".".join(str(new_version)))
