21d890a2f8481b93eccf0c70bfaedda217db57cb
slavcho179 <slavidimitrov06@abv.bg>
2023-10-18 18:04:01 +0300

    Uploading list advanced

--- ListAdvanced/electron_distribution.py
+++ ListAdvanced/electron_distribution.py
@@ -1,13 +1,14 @@
 number_of_electrons = int(input())
-shell_list = []
+shell_lists = []
 for shell in range(1, number_of_electrons + 1):
     maximum_electrons_in_current_shell = 2 * shell ** 2
     if number_of_electrons >= maximum_electrons_in_current_shell:
-        shell_list.append(maximum_electrons_in_current_shell)
+        shell_lists.append(maximum_electrons_in_current_shell)
         number_of_electrons -= maximum_electrons_in_current_shell
         if number_of_electrons == 0:
             break
     else:
-        shell_list.append(number_of_electrons)
+        shell_lists.append(number_of_electrons)
         break
-print(shell_list)
+
+print(shell_lists)
