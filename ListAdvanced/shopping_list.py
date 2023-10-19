07ffc0ce359aa65433e10a5d6c32984fd032c3de
slavcho179 <slavidimitrov06@abv.bg>
2023-10-19 21:54:51 +0300

    Uploading list advanced

--- ListAdvanced/shopping_list.py
+++ ListAdvanced/shopping_list.py
@@ -4,27 +4,29 @@ command = input()
 while True:
     if command == 'Go Shopping!':
         break
-    command = command.split()
-    type_of_command = command[0]
-    if type_of_command == 'Urgent':
-        item = command[1]
-        if item not in groceries_list:
-            groceries_list.insert(item, 0)
+    command_parts = command.split()
+    action = command_parts[0]
 
-    elif type_of_command == 'Unnecessary':
-        item = command[1]
+    if action == 'Urgent':
+        item = command_parts[1]
+
+        if item not in groceries_list:
+            groceries_list.insert(0, item)
+
+    elif action == 'Unnecessary':
+        item = command_parts[1]
         if item in groceries_list:
             groceries_list.remove(item)
 
-    elif type_of_command == 'Correct':
-        old_item = command[1]
-        new_item = command[2]
+    elif action == 'Correct':
+        old_item = command_parts[1]
+        new_item = command_parts[2]
         if old_item in groceries_list:
             index = groceries_list.index(old_item)
             groceries_list[index] = new_item
 
-    elif type_of_command == 'Rearrange':
-        item = command[1]
+    elif action == 'Rearrange':
+        item = command_parts[1]
         if item in groceries_list:
             groceries_list.remove(item)
             groceries_list.append(item)
