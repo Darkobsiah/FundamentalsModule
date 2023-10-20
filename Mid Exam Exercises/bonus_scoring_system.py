ad5f22c231d7969134a7f91590b49e4d2e74b848
slavcho179 <slavidimitrov06@abv.bg>
2023-10-20 23:29:21 +0300

    uploading mid-exam exercise solution

---
+++ Mid Exam Exercises/bonus_scoring_system.py
@@ -0,0 +1,15 @@
+number_of_students = int(input())
+number_of_lectures = int(input())
+additional_bonus = int(input())
+max_bonus = 0
+max_attendances = 0
+
+for student in range(number_of_students):
+    current_student_attendances = int(input())
+    current_student_bonus = current_student_attendances / number_of_lectures * (5 + additional_bonus)
+    if current_student_bonus > max_bonus:
+        max_bonus = round(current_student_bonus)
+        max_attendances = current_student_attendances
+
+print(f"Max Bonus: {max_bonus}.")
+print(f"The student has attended {max_attendances} lectures.")
