3c1a8094bafc49009f3ea5753254b19cb930671e
slavcho179 <slavidimitrov06@abv.bg>
2023-10-21 05:25:16 +0300

    adding useful comments

--- Mid Exam Exercises/memory_game.py
+++ Mid Exam Exercises/memory_game.py
@@ -2,6 +2,7 @@ def main():
     sequence_of_elements = input().split()
     count_moves = 0
 
+    # Initialize a for loop
     while True:
         count_moves += 1
         command = input()
@@ -17,7 +18,8 @@ def main():
             handle_valid_input(index1, index2, sequence_of_elements, count_moves)
 
 
-def is_invalid_input(index1, index2, sequence):
+# Define a function that checks for invalid input
+def is_invalid_input(index1: int, index2: int, sequence):
     # If one of the following checks is true the function returns True
     # From then we step into (handle_invalid_input) function an so on
     return(
@@ -29,14 +31,16 @@ def is_invalid_input(index1, index2, sequence):
     )
 
 
-def handle_invalid_input(sequence, count_moves):
+# Define a function that handles the invalid input
+def handle_invalid_input(sequence: list, count_moves: int):
     mid_index = len(sequence) // 2
     sequence.insert(mid_index, f'-{count_moves}a')
     sequence.insert(mid_index, f'-{count_moves}a')
     print("Invalid input! Adding additional elements to the board")
 
 
-def handle_valid_input(index1, index2, sequence, count_moves):
+# Define a function that handles the valid input
+def handle_valid_input(index1: int, index2: int, sequence: list, count_moves: int):
     if sequence[index1] == sequence[index2]:
         print(f"Congrats! You have found matching elements - {sequence[index1]}!")
         second_el = sequence[index2]
