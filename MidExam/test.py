b7a80fe774dd9deeb753ae2128ca604b6085312a
slavcho179 <slavidimitrov06@abv.bg>
2023-10-22 15:40:31 +0300

    Testing parts of the code in this file

---
+++ MidExam/test.py
@@ -0,0 +1,14 @@
+number_of_coffees = 1
+
+coffee_list = [1, 2, 3, 4, 5]
+another_list = coffee_list.copy()
+coffee_list.reverse()
+
+counter = 0
+for drink in coffee_list:
+    another_list.remove(drink)
+    counter += 1
+    if counter == number_of_coffees:
+        break
+another_list.reverse()
+print(another_list)
