e7e3cb62df1d02a9533f7111e457fd021fa5c13d
slavcho179 <slavidimitrov06@abv.bg>
2023-10-26 15:19:41 +0300

    Uploading email object solution

--- OOP/email_class.py
+++ OOP/email_class.py
@@ -1,4 +1,3 @@
-# Create the class
 class Email:
     def __init__(self, sender, receiver, content):
         self.sender = sender
@@ -9,19 +8,17 @@ class Email:
     def send(self):
         self.is_sent = True
 
-    def get_into(self):
+    def get_info(self):
         return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
 
-final_emails = []
-info = input()
-while info != "Stop":
-    sender, receiver, content = info.split()
-    email_info = Email(sender, receiver, content)
-    final_emails.append(email_info)
-    info = input()
+output_list = []
+command = input()
+while command != "Stop":
+    sender, receiver, content = command.split()
+    output_list.append(Email(sender, receiver, content))
+    command = input()
 indexes = [int(index) for index in input().split(', ')]
 for index in indexes:
-    final_emails[index].send()
-for current_email in final_emails:
-    print(current_email.get_into())
-
+    output_list[index].send()
+for list in output_list:
+    print(list.get_info())
