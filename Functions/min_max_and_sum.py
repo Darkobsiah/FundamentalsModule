73916b73444f84fd65f714869f32f8b8ed0abdf2
slavcho179 <slavidimitrov06@abv.bg>
2023-10-26 12:30:59 +0300

    adding blank lines

--- Functions/min_max_and_sum.py
+++ Functions/min_max_and_sum.py
@@ -8,11 +8,12 @@ def min_max_sum(string_list: list):
     sum_value = sum(list_with_integers)
     return min_value, max_value, sum_value
 
+
 # Read User input
 input_list = input().split()
-min, max, sum = min_max_sum(input_list)
-print(f'The minimum number is {min}')
-print(f'The maximum number is {max}')
-print(f'The sum number is: {sum}')
+minimum, maximum, sum_t = min_max_sum(input_list)
 
 # Print result from the function
+print(f'The minimum number is {minimum}')
+print(f'The maximum number is {maximum}')
+print(f'The sum number is: {sum_t}')
