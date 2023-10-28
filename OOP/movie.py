447cfaaf47c8da89079427995c3388d8beea48be
slavcho179 <slavidimitrov06@abv.bg>
2023-10-28 16:48:21 +0300

    OOP exercise solution upload

---
+++ OOP/movie.py
@@ -0,0 +1,40 @@
+class Movie:
+    __watched_movies = 0
+
+    def __init__(self, name: str, director: str):
+        self.name = name
+        self.director = director
+        self.watched = False
+        self.watched_movies = []
+
+    # Changes the name of the movie
+    def change_name(self, new_game: str):
+        self.name = new_game
+
+    # Changes the director of the movie
+    def change_director(self, new_director: str):
+        self.director = new_director
+
+    # Set the bool to True and increase the counter
+    def watch(self):
+        if self.name is not self.watched_movies:
+            if self.watched is not True:
+                self.watched = True
+                Movie.__watched_movies += 1
+
+    def __repr__(self):
+        return f"Movie name: {self.name}; Movie director:" \
+               f" {self.director}.Total watched movies: {Movie.__watched_movies}"
+
+
+first_movie = Movie("Inception", "Christopher Nolan")
+second_movie = Movie("The Matrix", "The Wachowskis")
+third_movie = Movie("The Predator", "Shane Black")
+first_movie.change_director("Me")
+third_movie.change_name("My Movie")
+first_movie.watch()
+third_movie.watch()
+first_movie.watch()
+print(first_movie)
+print(second_movie)
+print(third_movie)
