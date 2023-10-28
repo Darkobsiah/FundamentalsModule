08fbb53b27cbb8a2a99a48b2a2907748ac7d049c
slavcho179 <slavidimitrov06@abv.bg>
2023-10-28 15:51:32 +0300

    hearth_figure

--- Functions/test.py
+++ Functions/test.py
@@ -1,8 +1,17 @@
-def palindrome_checker(list):
-    for num in list:
-        is_palindrome = num[0] == num[-1]
-        print(is_palindrome)
-
-
-positive_ints = input().split(", ")
-print(palindrome_checker(positive_ints))
+import math
+from turtle import *
+def hearta(k):
+    return 15*math.sin(k)**3
+def heartb(k):
+    return 12*math.cos(k)-5*\
+            math.cos(2*k)-2*\
+            math.cos(3*k)-\
+            math.cos(4*k)
+speed(0)
+bgcolor("black")
+for i in range(6000):
+    goto(hearta(i)*20,heartb(i)*20)
+    for j in range(5):
+        color("#f73487")
+    goto(0,0)
+done()
