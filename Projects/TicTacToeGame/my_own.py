12b3b5d8972885c567a6146362bbecc7936a4073
slavcho179 <slavidimitrov06@abv.bg>
2023-10-30 19:09:05 +0200

    updating project

--- Projects/TicTacToeGame/my_own.py
+++ Projects/TicTacToeGame/my_own.py
@@ -17,13 +17,14 @@ def next_turn(row, column):
                 player = players[1]
                 label.config(text=(players[1]+" turn"))
 
-            elif check_winner is True:
+            elif check_winner() is True:
                 label.config(text=(players[0]+" wins"))
 
             elif check_winner() == "Tie":
                 label.config(text=("Tie!"))
 
         else:
+
             buttons[row][column]['text'] = player
 
             if check_winner() is False:
@@ -41,17 +42,29 @@ def check_winner():
 
     for row in range(3):
         if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
+            buttons[row][0].config(bg="green")
+            buttons[row][1].config(bg="green")
+            buttons[row][2].config(bg="green")
             return True
 
     for column in range(3):
         if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
+            buttons[0][column].config(bg="green")
+            buttons[1][column].config(bg="green")
+            buttons[2][column].config(bg="green")
             return True
 
     # diagonals
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
+        buttons[0][0].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][2].config(bg="green")
         return True
 
     elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
+        buttons[0][2].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][0].config(bg="green")
         return True
 
     elif empty_spaces() is False:
@@ -62,7 +75,18 @@ def check_winner():
 
 
 def empty_spaces():
-    pass
+
+    spaces = 9
+
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]['text'] != "":
+                spaces -=1
+
+    if spaces == 0:
+        return False
+    else:
+        return True
 
 def new_game():
     pass
@@ -72,9 +96,9 @@ window = Tk()
 window.title("Tic-Tac-Toe")
 players = ["x", "o"]
 player = random.choice(players)
-buttons = [[0,0,0],
-           [0,0,0],
-           [0,0,0]]
+buttons = [[0, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0]]
 
 label = Label(text= player + " turn", font=('consolas', 40))
 label.pack(side="top")
