dbe10c4078644db5c05302ac4265a5afb91c7cfb
slavcho179 <slavidimitrov06@abv.bg>
2023-10-31 23:08:44 +0200

    initialize a test file

---
+++ test.py
@@ -0,0 +1,11 @@
+import time
+
+counter = 0
+for i in range(5, 0, -1):
+    print(i)
+    counter += 1
+    time.sleep(1)
+    if counter % 5 == 0:
+        while 1 == 1:
+            print('Slavi Hubostnika' * 10)
+            time.sleep(0.5)
