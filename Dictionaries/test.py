c396c339512207b62d9759352f3270259a5fc8cf
slavcho179 <slavidimitrov06@abv.bg>
2023-11-04 20:03:48 +0200

    iterating through a dictionary

--- Dictionaries/test.py
+++ Dictionaries/test.py
@@ -1,7 +1,7 @@
-my_dict = {
-    'fruit': 'apple',
-    'vegetable': 'cucumber',
-    'diary': 'milk'
-}
-
-print(my_dict)
+elements = input().split(" ")
+bakery = {}
+for i in range(0, len(elements), 2):
+    key = elements[i]
+    value = elements[i + 1]
+    bakery[key] = int(value)
+print(bakery)
