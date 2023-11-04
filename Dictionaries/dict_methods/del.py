6d6a3d9061bd71882b6f3299ab42fd069f6d9494
slavcho179 <slavidimitrov06@abv.bg>
2023-11-04 20:58:15 +0200

    adding uses

--- Dictionaries/dict_methods/del.py
+++ Dictionaries/dict_methods/del.py
@@ -4,3 +4,13 @@
 students = {'name': 'George', 'course': 'Fundamentals'}
 del students['course']
 print(students)
+
+
+# keyword can also delete the dictionary completely
+students = {
+    'first': 'Petkan',
+    'second': 'Dragan',
+    'third': 'Gosho'
+}
+del students
+# print(students) --> NameError
