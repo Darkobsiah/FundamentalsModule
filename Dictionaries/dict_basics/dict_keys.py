c27ea55736cb6c5e7f5fa3012489afcd8adf52b0
slavcho179 <slavidimitrov06@abv.bg>
2023-11-05 00:36:54 +0200

    upload data

--- Dictionaries/dict_basics/dict_keys.py
+++ Dictionaries/dict_basics/dict_keys.py
@@ -3,7 +3,7 @@ my_dict = {'name': 'Jack', 'age': 26}
 # Key can be used either inside square brackets or
 # with the get() method
 print(my_dict['name'])
-print(my_dict.get('age'))
+my_dict.get('age')
 
 # The return is NONE because
 # the key does not exists
