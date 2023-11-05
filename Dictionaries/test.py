faca304f5ce4715452a098df63a985ef6b8ed79a
slavcho179 <slavidimitrov06@abv.bg>
2023-11-05 10:41:40 +0200

    del

--- Dictionaries/test.py
+++ Dictionaries/test.py
@@ -1,13 +1,1 @@
-people = {
-    'Slavi': {'height': 185, 'weight': 72},
-    'Ivan': {'height': 184, 'weight': 74},
-    'Nasko': {'height': 178, 'weight': 73}
-}
 
-for name, info in people.items():
-    print(f'---{name}---')
-    for param, value in info.items():
-        if param == 'height':
-            print(f'{param} - {value}sm')
-        else:
-            print(f'{param} - {value}kg')
