150722fa70ac1894fd65a650d0cc3a7ee6da8902
slavcho179 <slavidimitrov06@abv.bg>
2023-11-07 16:55:41 +0200

    upload file

---
+++ Dictionaries/miner_task.py
@@ -0,0 +1,15 @@
+# Initialise a While loop
+counter = 0
+
+while True:
+    command = input()
+    counter += 1
+
+    # implement ending condition
+    if command == 'stop':
+        break
+
+
+
+
+# Print the output
