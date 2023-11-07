759d0ef924b787020cbc8f2bf12a32874aa9056e
slavcho179 <slavidimitrov06@abv.bg>
2023-11-07 17:02:06 +0200

    upload file

--- Dictionaries/miner_task.py
+++ Dictionaries/miner_task.py
@@ -1,15 +1,27 @@
 # Initialise a While loop
 counter = 0
+miner_dictionary = {}
+last_command = ''
 
 while True:
     command = input()
+    # increment by 1 with every input
     counter += 1
 
     # implement ending condition
     if command == 'stop':
         break
 
-
-
+    if counter % 2 != 0:
+        # resource one (even)
+        miner_dictionary[command] = ''
+    else:
+        # if counter is even
+        # adding the quantity of the given resource
+        miner_dictionary[last_command] = command
+    # save the name of the resource
+    last_command = command
 
 # Print the output
+for key, value in miner_dictionary.items():
+    print(f'{key} -> {value}')
