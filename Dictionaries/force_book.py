f61c2bbe0d173eaebdabc0078dc594522fe25247
slavcho179 <slavidimitrov06@abv.bg>
2023-11-08 22:17:07 +0200

    upload 100/100 solution

--- Dictionaries/force_book.py
+++ Dictionaries/force_book.py
@@ -1,35 +1,33 @@
-force_side_users = {}
+force_side_dictionary = {}
 command = input()
-
 while command != 'Lumpawaroo':
-
     if '|' in command:
         force_side, force_user = command.split(" | ")
-        if force_side_users:
-            for key, value in force_side_users.items():
-                if force_user in value:
-                    break
-                if force_side not in key:
-                    force_side_users[force_side] = []
-                    force_side_users[force_side].append(force_user)
-                    break
-        else:
-            force_side_users[force_side] = []
-            force_side_users[force_side].append(force_user)
+        user_is_part_of_the_force = False
+        for value in force_side_dictionary.values():
+            if force_user in value:
+                user_is_part_of_the_force = True
+                break
+
+        if not user_is_part_of_the_force:
+            if force_side not in force_side_dictionary.keys():
+                force_side_dictionary[force_side] = []
+            force_side_dictionary[force_side].append(force_user)
 
     elif '->' in command:
         force_user, force_side = command.split(" -> ")
-        for value in force_side_users.values():
-            if force_user not in value:
-                force_side_users[force_side] = []
-            else:
-                force_side_users[force_side] = force_side
-                print(f'{force_user} joins the {force_side} side!')
-            force_side_users[force_side].append(force_user)
+        for value in force_side_dictionary.values():
+            if force_user in value:
+                value.remove(force_user)
+                break
+        if force_side not in force_side_dictionary.keys():
+            force_side_dictionary[force_side] = []
+        force_side_dictionary[force_side].append(force_user)
+        print(f'{force_user} joins the {force_side} side!')
     command = input()
 
-for key, value in force_side_users.items():
-    if key:
-        print(f'Side: {key}, Members: {len(value)}')
-        for s in value:
-            print(f'! {s}')
+for force_side, force_users in force_side_dictionary.items():
+    if len(force_users) > 0:
+        print(f"Side: {force_side}, Members: {len(force_users)}")
+        for force_user in force_users:
+            print(f"! {force_user}")
