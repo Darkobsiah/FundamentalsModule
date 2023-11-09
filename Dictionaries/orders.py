67bc7088b2ad7e8837fd6901eaa58f3e3e287029
slavcho179 <slavidimitrov06@abv.bg>
2023-11-09 18:18:56 +0200

    i will never give up

--- Dictionaries/orders.py
+++ Dictionaries/orders.py
@@ -2,17 +2,25 @@ order_dictionary = {}
 
 command = input()
 while command != 'buy':
-    name, price, quantity = command.split()
+    name, (price), quantity = command.split()
     if name not in order_dictionary:
-        order_dictionary[name] = [price, quantity]
+        order_dictionary[name] = [float(price), quantity]
     else:
-        order_dictionary[name][quantity] += quantity
-        order_dictionary[name][price] = price
+        order_dictionary[name][0][1] += quantity
+        order_dictionary[name][1] = price
 
     command = input()
 
 for name, lst in order_dictionary.items():
     values = ''
-    print(f'{name} -> {q * p}')
+    money = 0
+    counter = 0
+    for value in lst:
+        if counter == 0:
+            money += float(value)
+        else:
+            money *= float(value)
+        counter += 1
+    print(f'{name} -> {money:.2f}')
 
 
