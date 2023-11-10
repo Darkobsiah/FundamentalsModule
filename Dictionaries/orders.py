fb313de62bccefa554ef79763f27e639e182a2d3
slavcho179 <slavidimitrov06@abv.bg>
2023-11-10 14:39:40 +0200

    upload solution of 06.Orders

--- Dictionaries/orders.py
+++ Dictionaries/orders.py
@@ -1,28 +1,32 @@
-order_dictionary = {}
+# Initialise a dict to store the data
+order_info = {}
 
-command = input()
-while command != 'buy':
-    name, price, quantity = command.split()
-    price = float(price)
-    quantity = float(quantity)
-    if name not in order_dictionary:
-        order_dictionary[name] = [price, quantity]
-    else:
-        order_dictionary[name][0] += price
-        order_dictionary[name][1] = quantity
-
+# Initialise a While loop
+while True:
     command = input()
 
-for name, lst in order_dictionary.items():
-    values = ''
-    money = 0
+    # break condition
+    if command == "buy":
+        break
+
+    # Split by space and pass numbers into float
+    name, price, quantity = command.split()
+    price, quantity = float(price), float(quantity)
+
+    if name not in order_info.keys():
+        order_info[name] = [price, 0]
+    order_info[name][1] += quantity
+    if price != order_info[name][0]:
+        order_info[name][0] = price
+
+for key, value in order_info.items():
+    total = 0
+    x = 0
     counter = 0
-    for value in lst:
+    for v in value:
         if counter == 0:
-            money += float(value)
+            x = v
         else:
-            money *= float(value)
+            total = x * v
         counter += 1
-    print(f'{name} -> {money:.2f}')
-
-
+    print(f"{key} -> {total:.2f}")
