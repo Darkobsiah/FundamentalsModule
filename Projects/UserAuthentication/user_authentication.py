f466019b0550928050523dd2d9a40817967bb0e4
slavcho179 <slavidimitrov06@abv.bg>
2023-11-10 22:39:04 +0200

    upload updates to a project

--- Projects/UserAuthentication/user_authentication.py
+++ Projects/UserAuthentication/user_authentication.py
@@ -1,8 +1,40 @@
+from cryptography.fernet import Fernet
+
 user_credentials = {}
 user_profile_info = {}
 
 
-def password_validator(user_pass: str):
+def save_credentials(user, password, credentials):
+    credentials[user] = {'pass': password}
+    print(credentials)
+    return True
+
+
+def encrypt_decrypt_messages(command, username, password, credentials):
+    if command == 'encrypt':
+        key = Fernet.generate_key()
+        fernet = Fernet(key)
+
+        # then use the Fernet class instance
+        # to encrypt the string string must
+        # be encoded to byte string before encryption
+        encMessage = fernet.encrypt(password.encode())
+
+        credentials[username] = {'key': key, 'encMessage': encMessage}
+        print(credentials)
+    else:
+        fernet = Fernet(credentials['key'])
+        # decrypt the encrypted string with the
+        # Fernet instance of the key,
+        # that was used for encrypting the string
+        # encoded byte string is returned by decrypt method,
+        # so decode it to string with decode methods
+        decMessage = fernet.decrypt(password).decode()
+        print(credentials)
+        print("decrypted string: ", decMessage)
+
+
+def password_validator(username: str, user_pass: str, credentials: dict):
     valid = True
     special = user_pass.lower()
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
@@ -24,16 +56,26 @@ def password_validator(user_pass: str):
         print('Password must have at least 2 digits')
         valid = False
     if valid:
-        return 'Password is valid'
+        print('Password is valid')
+        return True
     else:
         return ''
 
 
-def main_menu():
+def main():
+    log_or_reg = input('Hello user, if you already have an account input /log/'
+                       'otherwise, write the following in the console /reg/')
+    if log_or_reg.lower() == 'log':
+        name = input('Name: ')
+        while True:
+            password = input('Enter password: ')
+
+            if password_validator(name, password, user_credentials):
+                break
+
+        save_credentials(name, password, user_credentials)
+    elif log_or_reg
 
 
-    # Read User input
-    password = input()
-
-    # Print the result of the function
-    print(password_validator(password))
+if __name__ == '__main__':
+    main()
