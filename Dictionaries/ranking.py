c07753dd9c7a6e1d77505f17328f5991deada17b
slavcho179 <slavidimitrov06@abv.bg>
2023-11-11 21:05:10 +0200

    upload file

--- Dictionaries/ranking.py
+++ Dictionaries/ranking.py
@@ -1,10 +1,27 @@
-some_dict = {}
+courses = {}
+
 
 while True:
     command = input()
     if command == 'end of contests':
         break
     contest, password = command.split(':')
-    some_dict[contest] = password
+    courses[contest] = password
+
+
+while True:
+    is_valid = True
+
+    command = input()
+    if command == 'end of submissions':
+        break
+
+    contest, password, username, points = command.split('=>')
+    if contest not in courses.keys():
+        is_valid = False
+    elif courses[contest] != password:
+        is_valid = False
+    else:
+
 
 
