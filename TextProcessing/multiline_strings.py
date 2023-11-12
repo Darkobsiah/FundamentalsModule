9cd8e5ba20054de839e5bd820913c4bfd0f6e0a8
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 19:25:37 +0200

    upload a file

---
+++ TextProcessing/multiline_strings.py
@@ -0,0 +1,18 @@
+use = """ This exact string type
+is being use mainly for explaining
+the functionality of some function.
+"""
+
+# EXAMPLE
+def string_concatenation(f_str: str,s_str: str) -> str:
+    """
+    This function take take two params:
+    Both of them - <str><type>
+
+    It concatenate them and returns:
+    output - <str>
+    """
+    final_output = f_str + s_str
+    return final_output
+
+print(string_concatenation('Stoqn ', 'Manqka'))
