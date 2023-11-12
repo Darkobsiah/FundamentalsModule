1deb704425d20db7fd64e24b58966309f64905bb
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 21:05:00 +0200

    upload a file

--- TextProcessing/substrings.py
+++ TextProcessing/substrings.py
@@ -1,7 +1,10 @@
+# Read User input
 first_string = input()
 second_string = input()
 
+# Initialise a while loop
 while first_string in second_string:
     second_string = second_string.replace(first_string, '')
 
+# Print User output
 print(second_string)
