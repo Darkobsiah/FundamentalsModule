8e81df5ce4f7e3cd06034107089069e252b9d473
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 20:44:12 +0200

    upload a file

---
+++ TextProcessing/string_methods.py
@@ -0,0 +1,13 @@
+data = input()
+
+print('Lowercase:', data.lower())
+print('Uppercase:', data.upper())
+
+print('If data is digit:', data.isdigit())
+print('If data is:', data.isalpha())
+
+greeting = ' hello '
+print(greeting.lstrip())   # removes the left space
+print(greeting.rstrip())  # removes the r space
+print(greeting.strip())   # remove the both spaces
+
