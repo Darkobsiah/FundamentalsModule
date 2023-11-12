7c1bf8cfd35aaa9ba4f48a8c93ae98a7debf5f2a
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 19:09:51 +0200

    del a file

--- TextProcessing/test.py
+++ TextProcessing/test.py
@@ -1,4 +1,11 @@
+# initialise a string
+a = 'Hello Wold Of Python'
+print(a)
 
-
+#  \t - adding more indentation
 a = 'Hello\t World\t Of\t Python'
+print(a)
+
+# \n - creates new line
+a = '\nHello Wold\nOf Python'
 print(a)
