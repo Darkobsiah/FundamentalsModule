46f32fdd4895a9745a42500bc4577a5343c6963e
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 19:48:17 +0200

    upload a file

--- TextProcessing/substring.py
+++ TextProcessing/substring.py
@@ -1,13 +1,11 @@
 a = 'Hello World of Python'
 
 # cut the beginning
-substring = a[6:]
-print(substring)
+substring = a[6:]   # World of Python
 
 # cut from the end
-substring = a[-6:]
-print(substring)
+substring_a = a[-6:]   # Python
 
 # reverse the string
-substring = a[::-1]
-print(substring)
+substring_b = a[::-1]   # nohtyP fo dlroW olleH
+
