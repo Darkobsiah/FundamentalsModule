6541fcbd504afdbf31d10de2c46ca75f6952dc80
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 19:57:38 +0200

    upload a file

--- TextProcessing/repeat_string.py
+++ TextProcessing/repeat_string.py
@@ -1,6 +1,12 @@
+# Read User input
 text = input().split()
 
+# initialise a list comprehension
 new_text = [txt * len(txt) for txt in text]
 
-for word in new_text:
-    print(word,end=' ')
+# the better variation
+print(' '.join(new_text))
+
+# OLD-FASHIONED
+# for word in new_text:
+#     print(word,end=' ')
