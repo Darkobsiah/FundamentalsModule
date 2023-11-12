c9b8d267de4e3a9f00cf3991e6fc55babc2d1ccf
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 19:43:41 +0200

    upload a file

---
+++ TextProcessing/substring.py
@@ -0,0 +1,13 @@
+a = 'Hello World of Python'
+
+# cut the beginning
+substring = a[6:]
+print(substring)
+
+# cut from the end
+substring = a[-6:]
+print(substring)
+
+# reverse the string
+substring = a[::-1]
+print(substring)
