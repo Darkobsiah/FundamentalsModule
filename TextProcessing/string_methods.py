baea3d51e106cf0e02b5d814b803f652d1918de5
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 20:49:56 +0200

    upload a file

--- TextProcessing/string_methods.py
+++ TextProcessing/string_methods.py
@@ -1,13 +1,22 @@
-data = input()
+data = 'TeXt'
 
 print('Lowercase:', data.lower())
+print('Lowercase:', data.casefold())  # just as lower()
 print('Uppercase:', data.upper())
+print()
 
 print('If data is digit:', data.isdigit())
-print('If data is:', data.isalpha())
+
+name = 'slavi'
+print(name.capitalize())   # first letter -> upper()
+print()
 
 greeting = ' hello '
 print(greeting.lstrip())   # removes the left space
 print(greeting.rstrip())  # removes the r space
 print(greeting.strip())   # remove the both spaces
+print()
 
+print(name.startswith('S'))
+print(name.capitalize().startswith('S'))
+print(name.endswith('i'))
