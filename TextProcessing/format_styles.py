a245623d8213a287df4ff0bcede3202d93809cb6
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 19:18:19 +0200

    upload a file

---
+++ TextProcessing/format_styles.py
@@ -0,0 +1,10 @@
+name = 'Ivan'
+age = 39
+
+# New formatting style
+text = 'My name is %s and I am %d years old.' % (name, age)
+print(text)
+
+# The old-fashioned one
+text = f'My name is {name} and I am {age} years old.'
+print(text)
