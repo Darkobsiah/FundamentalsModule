968f253c83a6577901db86e7f02f14594870b044
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 21:49:49 +0200

    upload a file

--- TextProcessing/valid_usernames.py
+++ TextProcessing/valid_usernames.py
@@ -2,22 +2,27 @@
 usernames = input().split(', ')
 valid_usernames = []
 
+# Initialise a for-loop
 for username in usernames:
     is_valid = True
     if 3 > len(username):
         is_valid = False
     elif 16 < len(username):
         is_valid = False
+    # is false - break condition
     if not is_valid:
         continue
 
+    # Initialise a nested for-loop
     for symbol in username:
         if symbol.isalpha() or symbol.isnumeric() or symbol == '-' or symbol == '_':
             continue
         else:
+            # is false - break condition
             is_valid = False
             break
     if is_valid:
         valid_usernames.append(username)
 
-print(' '.join(valid_usernames))
+# Print User output
+print('\n'.join(valid_usernames))
