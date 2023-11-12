9bb1305a204e8a3bb2138c3a3ccdbc18fe51cd33
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 19:27:15 +0200

    upload a file

--- TextProcessing/format_styles.py
+++ TextProcessing/format_styles.py
@@ -8,3 +8,9 @@ print(text)
 # The old-fashioned one
 text = f'My name is {name} and I am {age} years old.'
 print(text)
+
+# Third one
+price = 20.5
+quantity = 10
+order = 'The total cost is ${:.2f} for {} items'.format(price * quantity, quantity)
+print(order)
