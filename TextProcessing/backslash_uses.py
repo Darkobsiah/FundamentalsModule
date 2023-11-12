09de6b31ad87286fd17f98310d13b39979ca0d99
slavcho179 <slavidimitrov06@abv.bg>
2023-11-12 19:33:01 +0200

    upload a file

---
+++ TextProcessing/backslash_uses.py
@@ -0,0 +1,15 @@
+# We use backslash to implement quotes within a string
+myself = 'I\'m the baddest mdf\'er'
+print(myself)
+
+# check the ID of the variable in the memory
+path = 'C/path/to/server'
+print(id(path))
+
+# Implement double quotes within single ones
+location = '"On the planet Earth"'
+print(location)
+
+# Or maybe the opposite
+location = "'In the bar tonight'"
+print(location)
