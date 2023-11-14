275efb3c9c5f962b1c9ec9b5a2c4ba4cd0880c22
slavcho179 <slavidimitrov06@abv.bg>
2023-11-14 17:05:20 +0200

    upload test file changes

--- Projects/UserAuthentication/test.py
+++ Projects/UserAuthentication/test.py
@@ -1,3 +1,6 @@
+from cryptography.fernet import Fernet
+
+
 def register_credentials(username, password, credentials):
     # generate a key for encryption and decryption
     # You can use fernet to generate
@@ -5,6 +8,7 @@ def register_credentials(username, password, credentials):
     # here I'm using fernet to generate key
 
     key = Fernet.generate_key()
+    print(key)
 
     # Instance the Fernet class with the key
     fernet = Fernet(key)
@@ -26,6 +30,35 @@ def register_credentials(username, password, credentials):
     # that was used for encrypting the string
     # encoded byte string is returned by decrypt method,
     # so decode it to string with decode methods
-    decMessage = fernet.decrypt(encMessage).decode()
-    print(credentials)
-    print("decrypted string: ", decMessage)
+
+    # decMessage = fernet.decrypt(encMessage).decode()
+    # print(credentials)
+    # print("decrypted string: ", decMessage)
+
+
+def check_for_user(name, password, credentials):
+    if name in credentials.keys():
+        # encrypt password
+        key = credentials[name]['key']
+        fernet = Fernet(key)
+        decMessage = fernet.decrypt(password).decode()
+        print(decMessage)
+
+        if credentials[name]['encMessage'] == decMessage:
+            print('Allowed access')
+        else:
+            print('Incorrect password.')
+    else:
+        print('There is no user with such a name.')
+
+
+credentials = {}
+name = 'slavi'
+password = '12345'
+register_credentials(name, password, credentials)
+
+name = 'slavi'
+
+password = '12345'
+
+check_for_user(name, password, credentials)
