8d4ec59693de999da106185412e132948871bff3
slavcho179 <slavidimitrov06@abv.bg>
2023-11-14 17:52:19 +0200

    upload a 100/100 solution

--- TextProcessing/valid_username_func.py
+++ TextProcessing/valid_username_func.py
@@ -1,24 +1,67 @@
-def valid_length(users: list, valid_names):
-    for name in users:
+def valid_length(users: list):
+    """
+    This functions check the valid of the string
+    within the list and returns it only with validated ones
+    :param users: lists
+    :return: list
+    """
+    loop_list = users.copy()
+    for name in loop_list:
         if 3 < len(name) < 16:
-            valid_names.append(name)
-        else:
             continue
+        else:
+           users.remove(name)
     return users
 
 
-def valid_contains(users: list, valid_names):
-    for user in users:
+def valid_contains(users: list):
+    """
+        This functions check what string in the list
+        contains and keep only these with letters and nums
+        :param users: lists
+        :return: list
+    """
+    loop_list = users.copy()
+    for user in loop_list:
         for letter in user:
-            print(letter)
+            if letter.isalnum() or letter == '-' or letter == '_':
+                continue
+            else:
+                users.remove(user)
+                break
+    return users
+
+
+def redundant_symbols_check(users: list):
+    """
+        This functions check if the string within the list
+        contains Space or not, returns a list only whit these
+        who does not
+        :param users: lists
+        :return: list
+    """
+    loop_list = users.copy()
+    for user in loop_list:
+        if ' ' in user:
+            users.remove(user)
+    return loop_list
 
 
 def username_validator(users: list):
-    valid_names = []
-    valid_length(users, valid_names)
-    valid_contains(users, valid_names)
+    """
+    This is our main function. Here we only
+    call the other one which are described above.
+    :param users: lists
+    :return: list
+    """
+    valid_length(users)
+    valid_contains(users)
+    redundant_symbols_check(users)
+    return users
 
 
 # Read User input
 usernames = input().split(', ')
-username_validator(usernames)
+valid_usernames = username_validator(usernames)
+# Print User output
+print('\n'.join(valid_usernames))
