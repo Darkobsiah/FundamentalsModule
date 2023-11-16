7c1c1f78c3d4617a33e2c525366603ecc3acc86d
slavcho179 <slavidimitrov06@abv.bg>
2023-11-16 23:48:34 +0200

    upload changes

--- TextProcessing/letters_change_numbers.py
+++ TextProcessing/letters_change_numbers.py
@@ -1,20 +1,55 @@
 # Read User input
 combination = input()
-total_sum = 0
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'v','k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
             'y', 'z']
 
-def number_in_alphabet(text, char, letter_list):
+def number_in_alphabet(text: str, char: str, letter_list: list) -> int:
+    is_found = False
     my_letter = text[char]
     my_letter.lower()
     counter = 0
-    for
+    for letter in alphabet:
+        if letter == my_letter:
+            is_found = True
+        counter += 1
+        if is_found:
+            return counter
+        return True
 
 
-for character in range(len(combination)):
-    if character[0].isupper():
-        num = number_in_alphabet(combination, character, alphabet)
-        total_sum //
+number = ''
+characters = ''
+# Found the letters and the numbers
+# in the string and initialise them
+# in different strings
+for index in combination:
+    if index.isnumeric():
+        number += index
+    else:
+        characters += index
+number = int(number)
+total_sum = number
+
+final_count = 0
+for character in characters:
+    if final_count == 0:
+        if character.isupper():
+            alphabetic_num = number_in_alphabet(combination, character, alphabet)
+            total_sum = total_sum // alphabetic_num
+        else:
+            alphabetic_num = number_in_alphabet(combination, character, alphabet)
+            total_sum += number * alphabetic_num
+    else:
+        if character.isupper():
+            total_sum -= combination[-1]
+        else:
+            total_sum += combination[-1]
+    final_count += 1
+
+print(total_sum)
+
+
+
 
