b1d0f1209fd35df2bc6ea0fd59968bd8f2d3b1a8
slavcho179 <slavidimitrov06@abv.bg>
2023-11-16 23:35:38 +0200

    upload changes

---
+++ TextProcessing/letters_change_numbers.py
@@ -0,0 +1,20 @@
+# Read User input
+combination = input()
+total_sum = 0
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'v','k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z']
+
+def number_in_alphabet(text, char, letter_list):
+    my_letter = text[char]
+    my_letter.lower()
+    counter = 0
+    for
+
+
+for character in range(len(combination)):
+    if character[0].isupper():
+        num = number_in_alphabet(combination, character, alphabet)
+        total_sum //
+
