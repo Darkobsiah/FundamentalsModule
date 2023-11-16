a75ef2b5362a013d0fa1c319fa4e5c778f88001c
slavcho179 <slavidimitrov06@abv.bg>
2023-11-16 23:11:22 +0200

    upload changes

--- TextProcessing/replace_repeating_chars.py
+++ TextProcessing/replace_repeating_chars.py
@@ -1,9 +1,12 @@
+# Read User input
 text = input()
 final_message = ''
 last_added_character = ''
+# Loop through the string
 for current_character in text:
     if current_character != last_added_character:
         final_message += last_added_character
         last_added_character = current_character
 
+# Print User output
 print(''.join(final_message))
