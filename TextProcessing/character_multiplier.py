694d8228b3f9e957d3a727c3203e8fa0565874d5
slavcho179 <slavidimitrov06@abv.bg>
2023-11-16 21:25:55 +0200

    upload changes

--- TextProcessing/character_multiplier.py
+++ TextProcessing/character_multiplier.py
@@ -1,2 +1,15 @@
+# Read User input
 first_s, second_s = input().split()
 total_sum = 0
+if len(first_s) > len(second_s):
+    for letter in range(len(second_s)):
+        total_sum += ord(first_s[letter]) * ord(second_s[letter])
+    diff = len(first_s) - len(second_s)
+    for let in range(len(first_s) - diff, len(first_s)):
+        total_sum += ord(first_s[let])
+elif len(first_s) < len(second_s):
+    for letter in range(len(second_s)):
+        total_sum += ord(first_s[letter]) * ord(second_s[letter])
+    diff = len(first_s) - len(second_s)
+    for let in range(len(first_s) - diff, len(first_s)):
+        total_sum += ord(first_s[let])
