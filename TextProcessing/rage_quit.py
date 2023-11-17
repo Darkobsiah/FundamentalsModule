753721a36f68dd794b894068046bf4163e3d18e4
slavcho179 <slavidimitrov06@abv.bg>
2023-11-17 20:50:30 +0200

    upload a file

--- TextProcessing/rage_quit.py
+++ TextProcessing/rage_quit.py
@@ -3,22 +3,27 @@ output_line = ''
 last_symbol = ''
 
 # Loop through the string
-counter = 1
 for character in series:
-    if character.isnumeric():
+    # Do not entry non-numeric and symbols who are already in the output
+    if character.isnumeric() or character.upper() in output_line:
         continue
     current_string = ''
     multiplication_number = 1
     # alphabetic character
-    if character.isalpha():
-        for letter in range(counter, len(series)):
-            if series[letter].isalpha():
-                current_string += series[letter]
-            else:
-                multiplication_number = int(series[letter])
-                break
-    output_line += character.upper() * multiplication_number
-    counter += 1
+    if not character.isnumeric():
+        current_string += character
+        is_failed = False
+        current_index = series.index(current_string)
+        while not is_failed:
+            for x in range(1, 100):
+                if not series[current_index + x].isnumeric():
+                    current_string += series[current_index + x]
+                else:
+                    multiplication_number = int(series[current_index + x])
+                    is_failed = True
+                    break
+        output_line += current_string.upper() * multiplication_number
 
+# Print User output
 print(f'Unique symbols used: {len(set(output_line))}')
 print(output_line)
