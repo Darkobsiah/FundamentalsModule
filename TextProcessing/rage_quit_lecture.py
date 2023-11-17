fe0e006b9a57575b0bb1f86f80d3f20ab7eff38a
slavcho179 <slavidimitrov06@abv.bg>
2023-11-18 01:24:23 +0200

    upload a file

--- TextProcessing/rage_quit_lecture.py
+++ TextProcessing/rage_quit_lecture.py
@@ -6,4 +6,10 @@ for index in range(len(text)):
     if not text[index].isdigit():  # We have non-numeric character
         current_symbol += text[index]
     else:  # We have digits here -> time for repetition
-        ...
+        for next_symbols in range(index, len(text)):
+            if not text[next_symbols].isdigit():
+                break
+            repetitions += text[next_symbols]
+        unique_symbols += current_symbol * int(repetitions)
+        current_symbol = ""
+        repetitions = ""
