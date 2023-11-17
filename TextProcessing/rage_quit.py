91dd0a3a160bf976cbde46eed77751d65a4b530c
slavcho179 <slavidimitrov06@abv.bg>
2023-11-17 20:30:56 +0200

    upload a file

--- TextProcessing/rage_quit.py
+++ TextProcessing/rage_quit.py
@@ -1,12 +1,24 @@
-series = input()
-
+series = [x for x in input()]
 output_line = ''
 last_symbol = ''
 
 # Loop through the string
+counter = 1
 for character in series:
+    if character.isnumeric():
+        continue
+    current_string = ''
+    multiplication_number = 1
     # alphabetic character
     if character.isalpha():
-        print(character)
-    else:
-        print(character)
+        for letter in range(counter, len(series)):
+            if series[letter].isalpha():
+                current_string += series[letter]
+            else:
+                multiplication_number = int(series[letter])
+                break
+    output_line += character.upper() * multiplication_number
+    counter += 1
+
+print(f'Unique symbols used: {len(set(output_line))}')
+print(output_line)
