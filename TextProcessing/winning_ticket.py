28c7b0931277c3a7930eb7a5a1412be25301ef95
slavcho179 <slavidimitrov06@abv.bg>
2023-11-17 18:47:42 +0200

    upload changes

--- TextProcessing/winning_ticket.py
+++ TextProcessing/winning_ticket.py
@@ -1,5 +1,14 @@
+# Read User input
 tickets = [ticket.strip() for ticket in input().split(', ')]
+valid_symbols = ['@', '#', '$']
 
+for ticket in tickets:
+    if len(ticket) == 20:
+        left_part = ticket[10:]
+        right_part = ticket[:10]
+        for symbol in valid_symbols:
+            for _ in range(10 + 1, 6):
+                print(symbol * _)
 
-
+# Print User output
 print(tickets)
