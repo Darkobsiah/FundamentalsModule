d3b0cdaa7b8991062fb932105d450e9f25e04409
slavcho179 <slavidimitrov06@abv.bg>
2023-11-17 20:22:15 +0200

    delete a file

---
+++ TextProcessing/rage_quit.py
@@ -0,0 +1,12 @@
+series = input()
+
+output_line = ''
+last_symbol = ''
+
+# Loop through the string
+for character in series:
+    # alphabetic character
+    if character.isalpha():
+        print(character)
+    else:
+        print(character)
