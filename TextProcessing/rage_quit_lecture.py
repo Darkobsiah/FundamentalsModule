986de50b3bf504501ea69c5b88c731c0d5012302
slavcho179 <slavidimitrov06@abv.bg>
2023-11-18 10:42:33 +0200

    upload a file

--- TextProcessing/rage_quit_lecture.py
+++ TextProcessing/rage_quit_lecture.py
@@ -10,6 +10,9 @@ for index in range(len(text)):
             if not text[next_symbols].isdigit():
                 break
             repetitions += text[next_symbols]
-        unique_symbols += current_symbol * int(repetitions)
+        unique_symbols += current_symbol.upper() * int(repetitions)
         current_symbol = ""
         repetitions = ""
+
+print(f'Unique symbols used: {len(set(unique_symbols))}')
+print(unique_symbols)
