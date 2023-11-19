9402a6f1987f0b302036577626f0bc1b35221831
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 17:32:03 +0200

    upload exercise solution

--- Regex/match_dates.py
+++ Regex/match_dates.py
@@ -2,10 +2,10 @@ import re
 
 text = input()
 regex_pattern = r'(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})'
-valid_data = re.finditer(regex_pattern, text)
+valid_data = re.findall(regex_pattern, text)
 
 for data in valid_data:
-    day = data.group(1)
-    month = data.group(3)
-    year = data.group(4)
-    print(f'Day: {day}, Month: {month}, Year: {year}')
+    d = data[0]
+    m = data[2]
+    y = data[3]
+    print(f'Day: {d}, Month: {m}, Year: {y}')
