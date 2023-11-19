064024d28a6ae8d8762daeb55a861e8c4d1f1631
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 17:12:06 +0200

    upload game.py

--- Regex/match_dates.py
+++ Regex/match_dates.py
@@ -1,10 +1,9 @@
 import re
 
 text = input()
-regex_pattern = r'(\d{2})([-.\/])([A-Z][a-z]{2}\2\d{4})'
+regex_pattern = r'(\d{2})([-./])([A-Z][a-z]{2})\2(\d{4})'
 valid_data = re.findall(regex_pattern, text)
 
-print(valid_data)
 for data in valid_data:
-    month, year = data[2].split()
-    print(f'Day: {data[0]} Month: {data[1]}, Year: {data[2]}')
+    print(f'Day: {data[0]}, Month: {data[2]}, Year: {data[3]}')
+
