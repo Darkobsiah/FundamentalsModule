7adf1150bda65abab72ccefce600c56bf26fbe9a
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 13:57:18 +0200

    upload testing file

--- Regex/starts_or_ends_with.py
+++ Regex/starts_or_ends_with.py
@@ -1,6 +1,12 @@
 import re
 
 text = 'Python is fun.'
-pattern = '^Python'
-result = re.findall(pattern, text)
+
+
+starts_pattern = '^Python'
+result = re.findall(starts_pattern, text)
+print(result)
+
+ends_pattern = 'fun\.$'
+result = re.findall(ends_pattern, text)
 print(result)
