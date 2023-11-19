3816d06d45c9e69a46fcae3b4e0de0ddd2894213
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 17:23:50 +0200

    upload exercise

--- Regex/match_dates.py
+++ Regex/match_dates.py
@@ -1,1 +1,11 @@
-# your turn boy
+import re
+
+text = input()
+regex_pattern = r'(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})'
+valid_data = re.finditer(regex_pattern, text)
+
+for data in valid_data:
+    day = data.group(1)
+    month = data.group(3)
+    year = data.group(4)
+    print(f'Day: {day}, Month: {month}, Year: {year}')
