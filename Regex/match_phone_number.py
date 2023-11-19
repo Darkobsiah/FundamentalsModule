93af0ef9b02e2ee40ee6b503ce3885cfb3e6693d
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 21:15:55 +0200

    upload exercise solution

--- Regex/match_phone_number.py
+++ Regex/match_phone_number.py
@@ -4,9 +4,9 @@ import re
 # Read User input
 numbers = input()
 # Regex about specific sequence of symbols.
-regex_template = r'\+359 \d{1} \d{3} \d{4}\b|\+359-\d{1}-\d{3}-\d{4}\b'
+regex_template = r'\+359 2 \d{3} \d{4}\b|\+359-2-\d{3}-\d{4}\b'
 # Find something specific in the string above.
 matches = re.findall(regex_template, numbers)
 
 # Print User output
-print(''.join(matches))
+print(', '.join(matches))
