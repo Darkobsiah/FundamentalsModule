104fc845dab3566673c5cd835fdec228126a19d5
slavcho179 <slavidimitrov06@abv.bg>
2023-11-20 00:07:28 +0200

    upload calendar module exercises

---
+++ password_breaker.py
@@ -0,0 +1,10 @@
+# List of often-used passwords
+password_to_break = ['123456', 'admin', '12345678',
+                     '123456789', '1234', '12345',
+                     'password', '123', 'Aa123456',
+                     '1234567890', 'UNKNOWN', '1234567',
+                     '123123', '111111', 'Password',
+                     '000000', 'admin123', '********', 'user']
+
+lower_letters = [chr(x) for x in range(ord('A'), ord('a') + 57)]
+print(lower_letters)
