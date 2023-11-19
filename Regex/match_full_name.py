49aa662fd30ea924a372bbf3fc47871ef433d257
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 22:12:33 +0200

    upload exercise solution

--- Regex/match_full_name.py
+++ Regex/match_full_name.py
@@ -4,7 +4,7 @@ import re
 # Read User input
 text = input()
 # Template for symbol sequence to search for
-regex_pattern = r'\b[A-Z][a-z]+ [A-Z][a-z]+\b'
+regex_pattern = r'b\[A-Z][a-z]+ [A-Z][a-z]+\b'
 # Get all the repetitions from the string above
 valid_names = re.findall(regex_pattern, text)
 
