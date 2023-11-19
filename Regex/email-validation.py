5e2d0fef23fbc3d3d5ce2d32e71c8d66dd9ed041
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 15:13:32 +0200

    upload email validator with regex

--- Regex/email-validation.py
+++ Regex/email-validation.py
@@ -2,7 +2,7 @@ import re
 
 email_date = ['valid123@email.bg', 'invalid*name@email.bg']
 
-pattern = r'^[a-zA-Z0-9]+@+[a-zA-Z]+\.[a-zA-Z]'
+pattern = r'^^[a-zA-Z0-9]+@+[a-zA-Z]+\.[a-zA-Z]'
 
 for email in email_date:
     if re.match(pattern, email):
