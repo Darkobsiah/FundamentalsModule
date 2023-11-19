f9d733dcd67046bb01cb7f243288c359978cb00b
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 17:12:39 +0200

    upload exercise

--- Regex/match_dates.py
+++ Regex/match_dates.py
@@ -1,9 +1,1 @@
-import re
-
-text = input()
-regex_pattern = r'(\d{2})([-./])([A-Z][a-z]{2})\2(\d{4})'
-valid_data = re.findall(regex_pattern, text)
-
-for data in valid_data:
-    print(f'Day: {data[0]}, Month: {data[2]}, Year: {data[3]}')
-
+# your turn boy
