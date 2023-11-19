dbac7295f50e7fc3ab9d94343fdf8c15d219cbe7
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 21:09:03 +0200

    upload exercise solution

--- Regex/match_phone_number.py
+++ Regex/match_phone_number.py
@@ -1,11 +1,10 @@
 import re
 
-numbers = input().split(', ')
-regex_template = r'\+359([ -])\d{1}\1\d{3}\1\d{4}\b'
 
-valid_numbers = []
-for number in numbers:
-    result = re.findall(regex_template, numbers)
-    if result:
-        valid_numbers.append(number)
-print(valid_numbers)
+numbers = input()
+
+regex_template = r'\+359 \d{1} \d{3} \d{4}\b|\+359-\d{1}-\d{3}-\d{4}\b'
+matches = re.findall(regex_template, numbers)
+
+# Print User output
+print(matches)
