1af49e4bc1a07cb4f9182f66e0583197b03c35f4
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 22:32:21 +0200

    upload exercise solution

---
+++ TextProcessing/create_a_framge.py
@@ -0,0 +1,8 @@
+def frame(text, char):
+    n = len(max(text, key=len)) + 4
+    return "\n".join( [char * n] +
+                      ["%s %s %s" % (char, line.ljust(n-4), char) for line in text] +
+                      [char * n])
+
+
+print(frame("okayy bro", '-'))
