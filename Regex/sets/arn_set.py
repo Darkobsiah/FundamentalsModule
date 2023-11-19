d9472380b88955d5dae76a5691375f2bba1aecc5
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 14:49:12 +0200

    upload exercise solution

--- Regex/sets/arn_set.py
+++ Regex/sets/arn_set.py
@@ -1,6 +1,6 @@
 import re
 
-text = 'Python is an interpreted language'
-pattern = '[abc]'
+text = '30-Dec-1993'
+pattern = r'\b\d{1,2}-[A-Z][a-z]{2}-\d{4}\b'
 result = re.findall(pattern, text)
 print(result)
