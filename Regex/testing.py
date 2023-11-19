864a938965c06c3ab45ef9b4b598059fddeaef0f
slavcho179 <slavidimitrov06@abv.bg>
2023-11-19 13:52:05 +0200

    upload regex grouping

--- Regex/testing.py
+++ Regex/testing.py
@@ -1,6 +1,6 @@
 import re
 
-text = 'The ball is red and big, end of text'
-pattern = '(red|blue) and (big|small)'
+text = '1234 123 12 1 12345'
+pattern = '\d{3,}'
 result = re.findall(pattern, text)
 print(result)
