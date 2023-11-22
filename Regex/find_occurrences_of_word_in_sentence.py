0dc453dd6434085f39867d7462987c224b4b2cfc
slavcho179 <slavidimitrov06@abv.bg>
2023-11-22 18:04:34 +0200

    upload regex exercise solution

--- Regex/find_occurrences_of_word_in_sentence.py
+++ Regex/find_occurrences_of_word_in_sentence.py
@@ -1,8 +1,11 @@
 import re
 
 
+# Read User input
 line = input()
 word = input()
+# Use variable in the regex (?i) - case insensitive
 pattern = fr"(?i)\b{word}\b"
 result = re.findall(pattern, line)
+# Print User output
 print(len(result))
