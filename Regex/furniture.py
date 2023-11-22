7a4c02235cbf3a7d983cf945c5be3efe858c2d71
slavcho179 <slavidimitrov06@abv.bg>
2023-11-22 19:28:57 +0200

    upload regex exercise solution

--- Regex/furniture.py
+++ Regex/furniture.py
@@ -2,22 +2,21 @@ import re
 
 
 # Initialise lists to store data
-total_cost = []
 furniture_list = []
+total_cost = 0
 # Regular expression
-regex = r">+([A-Za-z]+)<<([0-9]+[\.0-9]*)!([0-9])"
-furniture = input()
-while furniture != 'Purchase':
-    item_info = re.search(regex, furniture)
-    if item_info:
-        item_name = item_info[1]
-        price = item_info[2]
-        quantity = item_info[3]
+regex = r">>([A-Za-z]+)<<(\d+\.?\d*)!(\d+)"
+command = input()
+while command != 'Purchase':
+    match = re.search(regex, command)
+    if match:
+        item_name, price, quantity = match.groups()
         furniture_list.append(item_name)
-        total_cost.append(float(price) * int(quantity))
-    furniture = input()
+        total_cost += float(price) * int(quantity)
+    command = input()
 
 # Print User output
 print('Bought furniture:')
-print('\n'.join(furniture_list))
-print(f'Total money spend: {sum(total_cost)}')
+for furniture in furniture_list:
+    print(furniture)
+print(f'Total money spend: {total_cost:.2f}')
