b6448e7edd04cac25699eb6e6f684cebc31464b5
slavcho179 <slavidimitrov06@abv.bg>
2023-11-24 12:43:56 +0200

    update a solution

--- password_breaker.py
+++ password_breaker.py
@@ -1,3 +1,5 @@
+import time
+start_time = time.time()
 # List of often-used passwords
 password_to_break = ['123456', 'admin', '12345678',
                      '123456789', '1234', '12345',
@@ -6,5 +8,25 @@ password_to_break = ['123456', 'admin', '12345678',
                      '123123', '111111', 'Password',
                      '000000', 'admin123', '********', 'user']
 
-lower_letters = [chr(x) for x in range(ord('A'), ord('a') + 57)]
-print(lower_letters)
+# Initialise the symbols
+all_symbols = []
+numbers = []
+lower_letters = [chr(x) for x in range(ord('A'), ord('a') + 26)]
+numbers += [str(x) for x in range(1, 11)]
+all_symbols += lower_letters
+all_symbols.extend(numbers)
+
+
+# Breaking algorithm
+for password in password_to_break:
+    guess = ''
+    loops_used = len(password)
+    for c in password:
+        for s in all_symbols:
+            if s == c:
+                guess += s
+    print(f'Password crashed: {guess}')
+
+print('-----Program Execution------')
+print(f"--- {(time.time() - start_time):.10f} seconds ---")
+
