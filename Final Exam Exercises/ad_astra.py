9f3d1fd69ecd48ef4a6b2bdad6b51eba56d9b630
slavcho179 <slavidimitrov06@abv.bg>
2023-11-26 19:38:13 +0200

    upload a solution for the exercise

--- Final Exam Exercises/ad_astra.py
+++ Final Exam Exercises/ad_astra.py
@@ -1,14 +1,21 @@
-item_credentials = {}
-
-text = '#Bread#19/03/21#4000#' \
-       '|Invalid|03/03.20|' \
-       '|Apples|08/10/20|200|' \
-       '|Carrots|06/08/20|500|' \
-       '|Not right|6.8.20|5|'
+import re
 
 
-text = text.split('#')
-item_credentials[text[1]] = {'expiration_date': text[2],
-                             'calories': text[3]}
-print(item_credentials)
+# Read User input
+food_info = input()
+regex = r"(#|\|)([A-Za-z\s]+)\1([\d\/]+)\1(\d+)\1"
+valid_info = re.findall(regex, food_info)
 
+# Initialise data
+calories_for_a_day = 2000
+food_calories = 0
+
+# Calculate food calories
+for item in valid_info:
+    food_calories += int(item[3])
+
+# Calculate for how much days the calories are going to end
+survive_days = food_calories // calories_for_a_day
+print(f"You have food to last you for: {survive_days}!")
+
+# Print every item and info for it
