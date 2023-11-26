572fae0d4ac756d5fbb61f28325d344ea4deb7d1
slavcho179 <slavidimitrov06@abv.bg>
2023-11-26 23:36:54 +0200

    upload update

--- Final Exam Exercises/the_pianist.py
+++ Final Exam Exercises/the_pianist.py
@@ -1,18 +1,34 @@
 def add_function(piece: str, composer: str, key: str, pieces: list):
-    if piece not in pieces:
+    is_there = False
+    for okayde in pieces:
+        for name, value in okayde.items:
+            print(key, value)
+
+    if not is_there:
         new_piece = {'piece': piece, 'composer': composer, 'key': key}
         print(f"{piece} by {composer} in {key} added to the collection!")
         return new_piece
     print(f"{piece} is already in the collection!")
     return False
 
+
+def remove_piece(piece: str, pieces: list):
+    if piece in pieces:
+        pieces.remove(piece)
+
+
 def main_func():
     num = int(input())
     piano_pieces = []
 
+    # Read User info
     for _ in range(num):
-        command = input()
+        part = input().split('|')
+        part, artist, key = part[0], part[1], part[2]
+        piano_pieces.append({'part': part, "artist": artist, "key": key})
 
+    while True:
+        command = input()
         if command == "Stop":
             break
         command = command.split("|")
@@ -24,4 +40,10 @@ def main_func():
             if element:
                 piano_pieces.append(element)
 
-        elif action == 'Remove'
+        elif action == 'Remove':
+            part = command[1]
+            remove_piece(part)
+
+
+if __name__ == '__main__':
+    main_func()
