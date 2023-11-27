5d1b63c8f94e433ba2b583c59ceee580c6af2be4
slavcho179 <slavidimitrov06@abv.bg>
2023-11-27 20:53:37 +0200

    upload solution

--- Final Exam Exercises/secret_chat.py
+++ Final Exam Exercises/secret_chat.py
@@ -1,10 +1,15 @@
 # Read User input
 conceal_message = input()
 
-command = input()
 # Initialise a while loop
-while command != "Reveal":
+while True:
+    command = input()
+    # Check if the word needs to be revealed
+    if command == "Reveal":
+        break
+    # Split the command
     command = command.split(":|:")
+
     if command[0] == 'InsertSpace':
         index = int(command[1])
         conceal_message = conceal_message[:index] + ' ' + conceal_message[index:]
@@ -14,9 +19,15 @@ while command != "Reveal":
         if substring in conceal_message:
             conceal_message = conceal_message.replace(substring, '')
             conceal_message += substring[::-1]
+        else:
+            print('error')
+            continue
+
     elif command[0] == 'ChangeAll':
         substring, replacement = command[1], command[2]
         conceal_message = conceal_message.replace(substring, replacement)
-    command = input()
+    # Print the changed message
+    print(conceal_message)
 
+# Print User output
 print(f'You have a new text message: {conceal_message}')
