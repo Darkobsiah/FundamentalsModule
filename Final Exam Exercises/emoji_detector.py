8eabe572cdb46ed88855be375c87ad8b30ef0cce
slavcho179 <slavidimitrov06@abv.bg>
2023-11-28 20:24:41 +0200

    upload exercise solution

--- Final Exam Exercises/emoji_detector.py
+++ Final Exam Exercises/emoji_detector.py
@@ -1,7 +1,9 @@
 import re
 
 
+# Read User input
 text = input()
+
 # Calculate the threshold
 threshold = 1
 number_reg = r"\d"
@@ -11,27 +13,24 @@ for number in numbers:
 print(f"Cool threshold: {threshold}")
 
 # Find the length of the emojis
-regex = r"([\::|\**]{2})([A-Z][a-z]{2,})\1"
-emojis_count = re.findall(regex, text)
+regex = r"([\:|\*]{2})([A-Z][a-z]{2,})\1"
+emojis_iterator = re.finditer(regex, text)
+# ([\:|\*]{2})[A-Z][a-z]{2,}\1
+
+emojis = []
+for e in emojis_iterator:
+    emojis.append(e.group())
 
 
-# Find the cool emojis
 cool_emojis = []
-text = text.split(' ')
-for word in text:
-    count = 0
-    match = re.search(regex, word)
-    if match:
-        for l in word:
-            if l.isalpha():
-                count += ord(l)
-        if count > threshold:
-            cool_emojis.append(word)
+for emoji in emojis:
+    counter = 0
+    for letter in emoji:
+        if letter.isalpha():
+            counter += ord(letter)
+    if counter > threshold:
+        cool_emojis.append(emoji)
 
-print(f'{len(emojis_count)} emojis found in the text. The cool ones are:')
+print(f'{len(emojis)} emojis found in the text. The cool ones are:')
 for emoji in cool_emojis:
-    if '.' in emoji:
-        emoji = emoji.replace('.', '')
-    elif ',' in emoji:
-        emoji = emoji.replace(',', '')
     print(emoji)
