bc62d68df244165e7b4416ef4bbe8b3d41b05969
slavcho179 <slavidimitrov06@abv.bg>
2023-11-29 16:01:52 +0200

    lambda function explanation #6

--- ListAdvanced/sorting_lists/lambda_test.py
+++ ListAdvanced/sorting_lists/lambda_test.py
@@ -7,3 +7,9 @@ print(square_numbers)
 strings = ['Pesho', 'gosho']
 lower_strings = list(map(lambda x: x.lower(), strings))
 print(lower_strings)
+
+upper_strings = list(map(lambda x: x.upper(), strings))
+print(upper_strings)
+
+capitalise_strings = list(map(lambda x: x.capitalize(), strings))
+print(capitalise_strings)
