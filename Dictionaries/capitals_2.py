55dd35063352012eed15cc13fcc7945934dfce2e
slavcho179 <slavidimitrov06@abv.bg>
2023-11-29 16:43:24 +0200

    upload dictionary exercise #3

---
+++ Dictionaries/capitals_2.py
@@ -0,0 +1,5 @@
+countries = [c for c in input().split(", ")]
+cities = [s for s in input().split(", ")]
+some_dict = dict(zip(countries, cities))
+for key,value in some_dict.items():
+    print(f"{key} -> {value}")
