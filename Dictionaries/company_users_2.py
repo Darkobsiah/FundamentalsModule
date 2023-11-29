56a588532015972e5907548a645c4d62a7407d76
slavcho179 <slavidimitrov06@abv.bg>
2023-11-30 00:10:09 +0200

    adding comments to #8

--- Dictionaries/company_users_2.py
+++ Dictionaries/company_users_2.py
@@ -1,16 +1,22 @@
+# Initialise a dict to store the data
 stored_data = {}
 
 # Read User input
 while True:
     command = input()
-    if command == 'End':
+    if command == 'End': # break condition
         break
     company, employee = command.split(" -> ")
+
     if company not in stored_data:
+        # Initialise a company space
         stored_data[company] = []
+
     if employee not in stored_data[company]:
+        # adding employees into the company list
         stored_data[company].append(employee)
 
+# Print User output
 for company, employees in stored_data.items():
     print(company)
     for employee in employees:
