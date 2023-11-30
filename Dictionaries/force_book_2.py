ce766be4bb06033a16967b33c0e6cafb4a8acfd5
slavcho179 <slavidimitrov06@abv.bg>
2023-11-30 23:56:53 +0200

    upload dictionary exercise #10

--- Dictionaries/force_book_2.py
+++ Dictionaries/force_book_2.py
@@ -10,17 +10,26 @@ while True:
         for value in force_book.values():
             if force_user in value:
                 is_there = True
+                break
 
         if not user_is_part_of_the_force:
-            if force_side not in force_book:
+            if force_side not in force_book.keys():
                 force_book[force_side] = []
             force_book[force_side].append(force_user)
 
     elif "->" in command:
         force_user, force_side = command.split(' -> ')
-        if force_user in force_book:
-            force_book.pop(force_user)
-            force_book[force_side].append(force_user)
-        else:
+        for value in force_book.values():
+            if force_user in value:
+                value.remove(force_user)
+                break
+
+        if force_side not in force_book.keys():
+            force_book[force_side] = []
+        force_book[force_side].append(force_user)
+        print(f"{force_user} joins the {force_side} side!")
+
+for side, members in force_book.items():
+    print(side, members)
 
 
