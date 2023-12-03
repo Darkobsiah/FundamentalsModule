bb42127c2c6950d650aa2b0bb4bb1d240381baae
slavcho179 <slavidimitrov06@abv.bg>
2023-12-03 14:37:20 +0200

    upload test file

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-import time
+names = ['Slavi', 'Nasko','Dobi']
 
-# initialize a counter
-counter = 0
-for i in range(5, 0, -1):
-    print(i)
-    counter += 1
-    time.sleep(1)
-    if counter % 5 == 0:
-        while 1 == 1:
-            print('Slavi Hubostnika' * 10)
-            print()
-            time.sleep(0.5)
+
+def name_validator(name):
+    if len(name) < 5:
+        print(f'{name} shorter than 5 symbols')
+        return False
+    return True
+
+
+
+access = filter(name_validator, names)
+for name in access:
+    print(f'{name} has access.')
 
 
