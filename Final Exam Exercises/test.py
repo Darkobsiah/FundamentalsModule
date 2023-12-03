435eb920485974e862222c9466191459af480209
slavcho179 <slavidimitrov06@abv.bg>
2023-12-03 13:33:15 +0200

    upload test file

--- Final Exam Exercises/test.py
+++ Final Exam Exercises/test.py
@@ -1,13 +1,3 @@
-# initialising dictionary
-ini_dict = {'nikhil': 1, 'vashu': 5,
-            'manjeet': 10, 'akshat': 15}
+string = '123456'
 
-# printing initial json
-print("initial 1st dictionary", ini_dict)
-
-# changing keys of dictionary
-ini_dict['akash'] = ini_dict['akshat']
-del ini_dict['akshat']
-
-# printing final result
-print("final dictionary", str(ini_dict))
+print(string[2:6])
