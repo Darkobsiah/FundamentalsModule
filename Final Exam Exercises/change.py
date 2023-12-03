2c6421e6efd890cd27f3773498505f3c14e5295f
slavcho179 <slavidimitrov06@abv.bg>
2023-12-03 11:28:49 +0200

    Upload the structure of the first exercise in my FE

---
+++ Final Exam Exercises/change.py
@@ -0,0 +1,22 @@
+# Read User input
+string = input()
+
+while True:
+    command = input().split()
+    if command == 'Done':
+        break
+    action = command[0]
+
+    if action == 'Change':
+        pass
+    elif action == 'Includes':
+        pass
+    elif action == 'End':
+        pass
+    elif action == 'Uppercase':
+        pass
+    elif action == 'FindIndex':
+        pass
+    elif action == 'Cut':
+        pass
+
