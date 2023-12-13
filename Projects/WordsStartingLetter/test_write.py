6e8f34255aa5e60d3ea9e84bd544315b629873d3
slavcho179 <slavidimitrov06@abv.bg>
2023-12-13 20:01:33 +0200

    upload project file

--- Projects/WordsStartingLetter/test_write.py
+++ Projects/WordsStartingLetter/test_write.py
@@ -1,10 +1,13 @@
-c = input('replace: ')
-r = input('with: ')
-f = open('sentences.txt', 'r')
-text = f.read()
-text = text.replace(c, r)
-f.close()
+words = ['first', 'second', 'third']
 
-f = open('sentences.txt', 'w')
-f.write(text)
-f.close()
+for word in words:
+    if word.startswith('s'):  # if match
+        print(f'Word or a phrase found: {word}')
+
+        # Write a sentence into the sentences.txt
+        file = open('sentences.txt', 'w')
+
+        sentence = input('Show imagination: ')
+        file.write(sentence)
+        file.close()
+        print()
