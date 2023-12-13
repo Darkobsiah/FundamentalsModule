a86b49803bc2aa7e4d55300918920657ee64ce51
slavcho179 <slavidimitrov06@abv.bg>
2023-12-13 19:19:05 +0200

    upload texting_files project manager

---
+++ Projects/WordsStartingLetter/replace_words.py
@@ -0,0 +1,10 @@
+c = input('replace: ')
+r = input('with: ')
+f = open('sentences.txt', 'r')
+text = f.read()
+text = text.replace(c, r)
+f.close()
+
+f = open('sentences.txt', 'w')
+f.write(text)
+f.close()
